96d91e5ec8c9e8de937b10c5f288f6c47f431cdf
Avik Roy Barman <50488111+arb-avikroy@users.noreply.github.com>
2025-12-30 07:27:55 +0000

    Final Commit after Database and bookmark tab creation. Addition of extra notes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// This app/website is build and modified using GPT-Agent and VSCode (To add features functionalities)
+// For any requirements or modificaitons reach out to "arb.avikroy@gmail.com/ or the Contact Form in the website"
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
